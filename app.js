7ba6ee742e68017eea1cc50af772e37a2d49599c
Nilesh Adhal <88049839+Nileshadhal@users.noreply.github.com>
2024-01-18 22:37:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,190 @@
+document.getElementById('myButton').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+
+document.getElementById('myButton1').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton2').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton3').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton4').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton5').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton6').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton7').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton8').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton9').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton10').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton11').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton12').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton13').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton14').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton15').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton16').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton17').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton18').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton19').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
+document.getElementById('myButton20').addEventListener('click', function () {
+    var loader = document.getElementById('loader');
+
+    loader.style.display = 'block';
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 3000); 
+});
